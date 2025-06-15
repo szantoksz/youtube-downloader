aa7bdda805c84b97d1149138be14d886c56720a2
szantoksz <151764223+szantoksz@users.noreply.github.com>
2025-06-16 01:13:25 +0200

    Deleted unneeded directory
    Don't worry about it :) TOOOOTAAALLLLYYYYY didn't push docker mounted test website files :3

--- src/client/srv/js/common/change_version_text.js
+++
@@ -1,4 +0,0 @@
-// define a function that takes an argument data and makes that the version number text
-export async function change_version_text(version) {
-  document.getElementById("version_number").textContent = version;
-}
